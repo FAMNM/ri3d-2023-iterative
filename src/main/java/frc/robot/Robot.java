7b34f0a633b10d7c682550721df24dd74cb27e7c
Connor Williams <Minisolarclown@users.noreply.github.com>
2023-01-07 21:36:06 -0500

    Added basic drive code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,25 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Robot extends TimedRobot {
+  WPI_VictorSPX left1 = new WPI_VictorSPX(1);
+  WPI_VictorSPX left2 = new WPI_VictorSPX(2);
+  WPI_VictorSPX right1 = new WPI_VictorSPX(3);
+  WPI_VictorSPX right2 = new WPI_VictorSPX(4);
+
+  MotorControllerGroup left = new MotorControllerGroup(left1, left2);
+  MotorControllerGroup right = new MotorControllerGroup(right1, right2);
+
+  DifferentialDrive mainDrive = new DifferentialDrive(left, right);
+
+  Joystick driver = new Joystick(0);
   
   /** This function is called once when the code starts */
   @Override
@@ -41,7 +57,15 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    
+
+    double x = driver.getRawAxis(0);
+    double y = driver.getRawAxis(1);
+
+    if(Math.sqrt(x*x + y*y) > 0.04) {
+      mainDrive.arcadeDrive(y, x);
+    } else {
+      mainDrive.arcadeDrive(0, 0);
+    }
   }
 
 }
